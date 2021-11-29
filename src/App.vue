87cc1eb69a970d1701cc12bbf51786de15c3cfe9
KamilShape <kamilshape@gmail.com>
2021-11-29 11:26:53 +0100

    adding search component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Characters</router-link> |
-    <router-link to="/favourites">Favourites</router-link>
+  <div class="nav">
+    <router-link class="nav_link" to="/">Characters</router-link> 
+    <router-link class="nav_link" to="/favourites">Favourites</router-link>
   </div>
-  <router-view />
+  <keep-alive>
+    <router-view />
+  </keep-alive>
 </template>
 
 <style lang="scss">
-
+.nav{
+  margin: 20px auto;
+  display: flex;
+  justify-content: space-around;
+  width: 350px;
+ 
+ 
+  &_link{
+    padding: 10px;
+    text-decoration: none;
+    background-color:rgb(110, 120, 255);
+     border-radius: 10px;
+    color: black;
+  }
+  
+}
 </style>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="characters">
-    <button @click="fetchCharacters">Fetch data</button>
-    <br>
+    <section class="characters_search">
+      <h2 class="characters_header">Search your favourites characters</h2>
+      <input class="characters_input" type="text">
+    </section>
+    <section class="characters_buttons">
+      <button @click="fetchCharacters" class="characters_button" >Show all</button>
+      <button @click="fetchCharacters" class="characters_button" >Search</button>
+    </section>
+  </div>
+  <keep-alive>
+    <div class="character_container">
     <Character v-for='character in characters' :key='character.id'
     :name='character.name'
     :species='character.species'
@@ -10,6 +19,8 @@
     :image='character.image'
     />
   </div>
+  </keep-alive>
+  
 </template>
 
 <script>
@@ -39,3 +50,49 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+  .characters{
+    width: 350px;
+    margin: 20px auto;
+    box-sizing: border-box;
+    padding: 20px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    &_header{
+      text-align: center;
+      padding: 20px;
+    }
+     &_input{
+      display: block;
+      width: 90%;
+      margin: 0 auto;
+      outline: none;
+      padding: 10px;
+      border: none;
+      text-align: center;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+    &_buttons{
+      display: flex;
+      justify-content: space-evenly;
+      margin: 10px auto;
+      width: 50%;
+    }
+    &_button{
+     font-size: 0.85rem;
+     padding: 5px;
+     border-radius: 5px;
+     margin-top: 5px;
+     border: none;
+     transition: 0.3s ease-in;
+     &:hover{
+       cursor: pointer;
+       color: white;
+       background-color: black;
+      }
+    }
+    &_container{
+      display: flex;
+    }
+   
+  }
+</style>
