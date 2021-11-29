6ea7a16483987c40d4e8eb3bed3af0845dec1987
KamilShape <kamilshape@gmail.com>
2021-11-29 10:16:49 +0100

    style character component

---
+++ src/components/Character.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="character">
+    <img class='character_image' :src="image">
+    <div class='character_container'>
+      <h4 class='character_name'>Name: {{name}}</h4>
+      <p class='character_description'>Status: {{status}}</p>
+      <p class='character_description'>Species: {{species}}</p>
+      <p class='character_description'>Gender: {{gender}}</p>
+      <button class='character_button'>Add to favourites</button>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Character",
+  props: {
+    name: String,
+    status: String,
+    species: String,
+    gender: String,
+    image: String
+  },
+};
+</script>
+
+<style lang="scss">
+ .character{
+   display: flex;
+   width: 350px;
+   margin: 20px auto;
+   box-sizing: border-box;
+   padding: 20px;
+   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+   &_image{
+     width: 150px;
+    height: 150px;
+   }
+   &_name{
+     font-size: 0.8rem;
+      width: 150px;
+   }
+   &_description{
+     font-size: 0.7rem;
+     width: 150px;
+   }
+   &_button{
+     font-size: 0.7rem;
+     padding: 5px;
+     border-radius: 5px;
+     margin-top: 5px;
+     border: none;
+     transition: 0.3s ease-in;
+     &:hover{
+       cursor: pointer;
+       color: white;
+       background-color: black;
+     }
+   }
+   &_container{
+     padding: 20px;
+   }
+ }
+</style>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="characters">
- 
+    <button @click="fetchCharacters">Fetch data</button>
+    <br>
+    <Character v-for='character in characters' :key='character.id'
+    :name='character.name'
+    :species='character.species'
+    :status='character.status'
+    :gender='character.gender'
+    :image='character.image'
+    />
   </div>
 </template>
 
 <script>
 
-
+import Character from'@/components/Character.vue'
 export default {
-  name: "Home",
-  components: {
-   
+  name: "Characters",
+  data(){
+    return{
+      characters: []
+    }
   },
+  components: {
+    Character
+  },
+  methods:{
+        async fetchCharacters(){
+          try {
+            let {data} = await this.axios('https://rickandmortyapi.com/api/character');
+            console.log(data.results)
+            this.characters = data.results
+          }
+          catch(e) {
+            console.log(e, 'Error')
+          }
+        }
+  }
 };
 </script>
